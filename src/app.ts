7414ba6fb76753d4543bef8e637719fc16ca6b92
Richmond <richmondclaudebodot@gmail.com>
2025-12-27 00:28:03 +0800

    Add CORS for security

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
+import cors from "cors";
 const app = express();
+
+const corsOptions = {
+  origin: process.env.ORIGIN_URL || 'http://localhost:3000', 
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
