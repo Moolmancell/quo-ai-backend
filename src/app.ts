3892bec2fb9ec00790fbcee94a3502f2f5cfbcc6
Richmond <richmondclaudebodot@gmail.com>
2025-12-26 21:21:20 +0800

    Remove login and signup routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
 import express from "express";
-import loginRouter from "./routes/loginRouter";
-import signUpRouter from "./routes/signupRouter";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 const app = express();
@@ -8,8 +6,4 @@ const app = express();
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 app.use(express.json());
-
-app.use("/api/login", loginRouter);
-app.use("/api/signup", signUpRouter);
-
 export default app;
--- src/routes/signupRouter.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signUpController } from "../controllers/signupController";
-
-const signUpRouter = express.Router();
-
-signUpRouter.post("/", signUpController);
-
-export default signUpRouter;
