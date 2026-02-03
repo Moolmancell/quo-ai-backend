2928780a69c60e4bca581bd2620b4ed78eaa9f6a
Richmond <richmondclaudebodot@gmail.com>
2026-02-03 17:09:46 +0800

    Remove TODOS

--- src/routes/interestsRouter.ts
+++ src/routes/interestsRouter.ts
@@ -2,11 +2,8 @@ import { Router } from "express";
 import { getInterests, submitInterests } from "../controllers/interestsController";
 const interestsRouter = Router();
 
-// TODO: Create /api/interests/get-interests/:userID endpoint
-//TODO: Create /api/interests/submit-interests/:userID endpoint
-//TODO: (OPTIONAL) Create /api/interests/generate-interests/:suserID endpoint (Transform.js for interest generation (Cloud Worker AI))
 interestsRouter.get("/get-interests", getInterests);
 interestsRouter.post("/submit-interests", submitInterests);
-interestsRouter.get("/generate-interests/:userID", (req, res) => res.send('generate-interests endpoint not implemented yet'));
+//TODO: (OPTIONAL) Create /api/interests/generate-interests/:suserID endpoint (Transform.js for interest generation (Cloud Worker AI))
 
 export default interestsRouter;
