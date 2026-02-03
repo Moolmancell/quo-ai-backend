4687fa2e3bf45072cae90448106915c84467ed59
Richmond <richmondclaudebodot@gmail.com>
2026-02-03 16:07:40 +0800

    Use getInterest function in /get-interests route

--- src/routes/interestsRouter.ts
+++ src/routes/interestsRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-
+import { getInterests } from "../controllers/interestsController";
 const interestsRouter = Router();
 
 // TODO: Create /api/interests/get-interests/:userID endpoint
 //TODO: Create /api/interests/submit-interests/:userID endpoint
-//TODO: Create /api/interests/generate-interests/:userID endpoint (Transform.js for interest generation (Cloud Worker AI))
-interestsRouter.get("/get-interests/:userID", (req, res) => res.send('get-interests endpoint not implemented yet'));
+//TODO: Create /api/interests/generate-interests/:suserID endpoint (Transform.js for interest generation (Cloud Worker AI))
+interestsRouter.get("/get-interests/:userID", getInterests);
 interestsRouter.post("/submit-interests/:userID", (req, res) => res.send('submit-interests endpoint not implemented yet'));
 interestsRouter.get("/generate-interests/:userID", (req, res) => res.send('generate-interests endpoint not implemented yet'));
 
