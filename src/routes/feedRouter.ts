edb93f76f26715a8270d1c5e36d85230bef6efe3
Richmond <richmondclaudebodot@gmail.com>
2026-05-25 16:43:32 +0800

    Remove TODO for /feed/get-feed

--- src/routes/feedRouter.ts
+++ src/routes/feedRouter.ts
@@ -3,7 +3,6 @@ const feedRouter = Router();
 import { getFeed } from '../controllers/feedController';
 
 //Feed Endpoints
-//TODO: Create /api/feed/get-feed/:userID endpoint (using LangChain to generate feed based on interests)
 //TODO: Create /api/feed/get-featured-image/:userID endpoint (use cloudinary for Image Proxy Buffer)
 //TODO: Create /api/feed/get-favicon-image/:userID endpoint
 //TODO: Create /api/feed/add-bookmark/:userID endpoint
